579448ac3bf2d5060c2f3f3bb0fb8d0d6694ba57
asta-otaku <afolabiibrahim08@gmail.com>
2026-07-06 11:46:07 +0100

    chore: Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -122,7 +122,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico" },
+      { url: "/logo.svg" },
       { url: "/icon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
